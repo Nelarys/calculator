d40fd784b39c482dabf08c22202011a05ec7b316
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-26 17:07:12 +0200

    feat: Add functions for basic math operations

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+let op1 = 0;
+let op2 = 0;
+let operator = "";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
